21b99197711e0c0c4b5977aa7e86ab07e45cde64
Jon Spriggs <jon@sprig.gs>
2020-01-02 11:19:51 +0000

    Removed obsolete setup scripts

--- .setup_tz_20191210.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DEBIAN_FRONTEND=noninteractive
-echo "tzdata tzdata/Areas select Etc" | debconf-set-selections
-echo "tzdata tzdata/Zones/Etc select UTC" | debconf-set-selections
-
-apt-get update
-apt-get install -y tzdata
